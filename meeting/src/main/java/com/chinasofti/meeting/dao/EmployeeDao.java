041d04defa5c964710c78cc66d1fdf5e4b44c8f3
勇敢的雪球 <wy03043355@163.com>
2020-08-10 11:21:54 +0800

    meeting v0.3.0 注册页面表单校验

--- meeting/src/main/java/com/chinasofti/meeting/dao/EmployeeDao.java
+++ meeting/src/main/java/com/chinasofti/meeting/dao/EmployeeDao.java
@@ -51,8 +51,44 @@ public class EmployeeDao {
 	
 	public static void main(String[] args) {
 		EmployeeDao dao = new EmployeeDao();
-		Employee emp = dao.selectByNamePwd("lilei", "123");
-		System.out.println(emp);
+		//Employee emp = dao.selectByNamePwd("lilei", "123");
+		Employee employee = dao.selectByUserName("alibaba");
+		System.out.println(employee);
+	}
+	/**
+	 *       根据用户名查找是否有对应的对象；
+	 * @param username 用户名
+	 * @return employee对象
+	 */
+	public Employee selectByUserName(String username) {
+		conn = ConnectionFactory.getConnection();
+		Employee employee = null;
+		PreparedStatement pstmt = null;
+		String sql = "select * from employee where username=?";
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				employee = new Employee();
+				employee.setEmployeeid(rs.getInt("employeeid"));
+				employee.setEmployeename(rs.getString("employeename"));
+				employee.setUsername(rs.getString("username"));
+				employee.setPhone(rs.getString("phone"));
+				employee.setEmail(rs.getString("email"));
+				employee.setStatus(rs.getString("status"));
+				employee.setDepartmentid(rs.getInt("departmentid"));
+				employee.setPassword(rs.getString("password"));
+				employee.setRole(rs.getString("role"));
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}finally {
+			ConnectionFactory.closeConnection(conn, pstmt, rs);
+		}
+		
+		return employee;
 	}
 	
 }
