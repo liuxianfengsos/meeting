cebad8b0b11e2685ea1c5d21d9b240982e03e5b3
勇敢的雪球 <wy03043355@163.com>
2020-08-12 14:40:27 +0800

    meeting v0.4.0完成注册审批页面分页显示

--- meeting/src/main/java/com/chinasofti/meeting/dao/EmployeeDao.java
+++ meeting/src/main/java/com/chinasofti/meeting/dao/EmployeeDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.chinasofti.meeting.util.ConnectionFactory;
 import com.chinasofti.meeting.vo.Employee;
@@ -48,15 +50,7 @@ public class EmployeeDao {
 		return employee;
 	}
 	
-	public static void main(String[] args) {
-		EmployeeDao dao = new EmployeeDao();
-		//Employee emp = dao.selectByNamePwd("lilei", "123");
-		//Employee employee = dao.selectByUserName("alibaba");
-		Employee e = new Employee(null, "呼呼啦", "hhl", "123", 1, "@@@", "1232132132", "0", "2");
-		dao.insert(e);
-		
-		//System.out.println(employee);
-	}
+	
 	/**
 	 *       根据用户名查找是否有对应的对象；
 	 * @param username 用户名
@@ -120,5 +114,57 @@ public class EmployeeDao {
 		
 		
 	}
+
+	public List<Employee> selectAllEmployee() {
+		conn = ConnectionFactory.getConnection();
+		List<Employee> employeeList = new ArrayList<Employee>();
+		Employee employee = null;
+		//查询等待审核的普通用户
+		String sql = "select * from employee where role='2' and status='0'";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				employee = new Employee();
+				employee.setEmployeeid(rs.getInt("employeeid"));
+				employee.setEmployeename(rs.getString("employeename"));
+				employee.setUsername(rs.getString("username"));
+				employee.setPhone(rs.getString("phone"));
+				employee.setEmail(rs.getString("email"));
+				employee.setStatus(rs.getString("status"));
+				employee.setDepartmentid(rs.getInt("departmentid"));
+				employee.setPassword(rs.getString("password"));
+				employee.setRole(rs.getString("role"));
+				employeeList.add(employee);
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}finally {
+			ConnectionFactory.closeConnection(conn, pstmt, rs);
+		}
+		
+		return employeeList;
+	}
 	
+	
+	public static void main(String[] args) {
+		EmployeeDao dao = new EmployeeDao();
+		//Employee emp = dao.selectByNamePwd("lilei", "123");
+		//Employee employee = dao.selectByUserName("alibaba");
+		//Employee e = new Employee(null, "呼呼啦", "hhl", "123", 1, "@@@", "1232132132", "0", "2");
+		//dao.insert(e);
+		List<Employee> list = dao.selectAllEmployee();
+		
+		for(Employee emp:list) {
+			
+			System.out.println(emp);
+		}
+		
+		
+		//System.out.println(employee);
+	}
 }
