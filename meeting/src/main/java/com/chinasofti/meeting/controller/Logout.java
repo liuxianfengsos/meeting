fcc99f7934cebbf51ea0eda60bb8eb33a4e63657
勇敢的雪球 <wy03043355@163.com>
2020-08-19 10:58:43 +0800

    meeting v0.6.7 退出

---
+++ meeting/src/main/java/com/chinasofti/meeting/controller/Logout.java
@@ -0,0 +1,28 @@
+package com.chinasofti.meeting.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Logout extends HttpServlet {
+
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		session.invalidate();
+		//request.getRequestDispatcher("../login.jsp").forward(request, response);
+		response.sendRedirect("../login.jsp");
+	}
+
+}
